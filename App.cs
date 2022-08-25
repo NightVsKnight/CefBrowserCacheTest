f0ff003d1403e2da2ac23609b3e1f122e8f7b2ef
NightVsKnight <night.vs.knight@gmail.com>
2022-08-25 10:57:01 -0700

    Add project files.

---
+++ App.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using CefSharp.WinForms;
+using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefBrowserCacheTest
+{
+    public class App : WindowsFormsApplicationBase
+    {
+        public static CefSettings CEF_SETTINGS
+        {
+            get;
+            private set;
+        }
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            CefInitialize();
+            new App()
+            {
+                EnableVisualStyles = true,
+                MainForm = new Form1()
+            }
+            .Run(args);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void CefInitialize()
+        {
+            CefRuntime.SubscribeAnyCpuAssemblyResolver();
+            var cachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"MyApp\CefSharp\Cache");
+            CEF_SETTINGS = new CefSettings()
+            {
+                CachePath = cachePath,
+                PersistSessionCookies = true,
+                PersistUserPreferences = true,
+            };
+            Cef.Initialize(CEF_SETTINGS, performDependencyCheck: true, browserProcessHandler: null);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using CefSharp.WinForms;
+using CefSharp;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CefBrowserCacheTest
+{
+    public partial class Form1 : Form
+    {
+        private ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Cef.IsInitialized)
+            {
+                throw new InvalidOperationException("Cef.Initialize(...) must be called first");
+            }
+
+            var appSettings = App.CEF_SETTINGS;
+            var cachePath = Path.Combine(appSettings.CachePath, "User_Profile");
+
+            RequestContextSettings requestContextSettings = new RequestContextSettings();
+            if (!String.IsNullOrEmpty(cachePath))
+            {
+                requestContextSettings.CachePath = cachePath;
+                requestContextSettings.PersistSessionCookies = true;
+                requestContextSettings.PersistUserPreferences = true;
+            }
+
+            browser = new ChromiumWebBrowser(String.Empty)
+            {
+                BrowserSettings = new BrowserSettings()
+                {
+                    LocalStorage = CefState.Enabled,
+                },
+                RequestContext = new RequestContext(requestContextSettings)
+            };
+
+            Controls.Add(browser);
+        }
+    }
+}
