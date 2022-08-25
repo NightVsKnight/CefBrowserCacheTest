b8cca37c23c6df64e4682c7907d318c6e433df12
NightVsKnight <night.vs.knight@gmail.com>
2022-08-25 11:21:38 -0700

    Attempt to simplify code even more

--- App.cs
+++ App.cs
@@ -2,37 +2,17 @@
 using CefSharp.WinForms;
 using CefSharp;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CefBrowserCacheTest
 {
     public class App : WindowsFormsApplicationBase
     {
-        public static CefSettings CEF_SETTINGS
-        {
-            get;
-            private set;
-        }
-
-        [STAThread]
-        private static void Main(string[] args)
-        {
-            CefInitialize();
-            new App()
-            {
-                EnableVisualStyles = true,
-                MainForm = new Form1()
-            }
-            .Run(args);
-        }
+        public static readonly CefSettings CEF_SETTINGS;
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        private static void CefInitialize()
+        static App()
         {
             CefRuntime.SubscribeAnyCpuAssemblyResolver();
             var cachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"MyApp\CefSharp\Cache");
@@ -43,6 +23,16 @@ namespace CefBrowserCacheTest
                 PersistUserPreferences = true,
             };
             Cef.Initialize(CEF_SETTINGS, performDependencyCheck: true, browserProcessHandler: null);
+        }
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            new App()
+            {
+                MainForm = new Form1()
+            }
+            .Run(args);
         }
     }
 }
